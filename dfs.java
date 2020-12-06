7a7e25a076967c689b4e395649cf6871a510f4fc
Dario Mazhara <leizaro.fei@gmail.com>
2020-12-06 14:07:28 -0800

    Create dfs.java

---
+++ dfs.java
@@ -0,0 +1,14 @@
+public class ClassName<E> extends AnyClass implements Interface<String> {
+enum Color { RED, GREEN, BLUE};
+static Object staticField;
+
+private E field;
+private AbstractClassName field2;
+
+public int foo(Integer param) {
+abstractMethod(inhertedField);
+int local = 42*hashCode();
+staticMethod();
+return bar(local) + parameter;
+}
+}
